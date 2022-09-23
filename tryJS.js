9443715f834eeab779530bd6a3587cf8c5991cba
Dorcas Chen <ruthkeyi@gmail.com>
2022-09-23 11:35:29 -0700

    js

---
+++ tryJS.js
@@ -0,0 +1,7 @@
+let userMusic = prompt("Do you like classical music?");
+
+if (userMusic.toLowerCase() == "yes"){
+    document.write("Me too!");
+} else {
+    document.write("I hope you will like his piece soon!");
+}
